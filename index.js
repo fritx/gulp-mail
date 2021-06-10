c7bbe892e7a095563e2bd19dda6315626be6d7b6
Jaroslav Vrana <jaroslav.vrana@integromat.com>
2021-06-10 22:56:49 +0200

    refactor: fixed gulp-util deprecations
    - Replaced `gulp-util.log` with `fancy-log`
    - Replaced `gulp-util.colors` with `ansi-colors`
    - Replaced `underscore` with `flat-options`
    - Added check if `options.to` is a single email or array of emails
    - Removed `_subjectFromFilename`; use `basename` if subject not provided
    
    Picked from 158c48fc75d0f86e643095320ae7b899485b54001 by justin713

--- index.js
+++ index.js
@@ -1,63 +1,54 @@
-// learnt from https://github.com/meerkats/gulp-mailer
-// used old nodemailer@0.7.1 instead
+'use strict';
 
-var _ = require('underscore');
+var colors = require('ansi-colors');
+var flatOpts = require('flat-options');
+var log = require('fancy-log');
 var nodemailer = require('nodemailer');
 var path = require('path');
 var through2 = require('through2');
-var util = require('util');
-var log = require('fancy-log');
-var colors = require('ansi-colors');
 
 module.exports = function (options) {
+  options = flatOpts(options, {
+    to: null,
+    from: null,
+    subject: null,
+    html: null,
+    text: null,
+    smtp: null,
+  });
 
-    options = _.defaults(options || {}, {
-        to: null,
-        from: null,
-        subject: null,
-        html: null,
-        text: null,
-        smtp: null
-    });
+  return through2.obj(function (file, enc, callback) {
+    var transporter = nodemailer.createTransport('SMTP', options.smtp);
 
-    return through2.obj(function (file, enc, next) {
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
+    }
 
-        var transporter = nodemailer.createTransport("SMTP", options.smtp);
+    var to = options.to.constructor === Array ? options.to.join(',') : options.to;
+    var subject = options.subject || path.basename(file.path);
+    var html = options.html || file.contents.toString();
+    var text = options.text || null;
 
-        if (file.isNull()) {
-            this.push(file);
-            return next();
+    return transporter.sendMail(
+      {
+        from: options.from,
+        to: to,
+        subject: subject,
+        generateTextFromHTML: true,
+        html: html,
+        text: text,
+      },
+      function (error, info) {
+        if (error) {
+          log.error(error);
+        } else {
+          log.info('Sent email', colors.cyan(subject), 'to', colors.red(to));
         }
 
-        var to = options.to.join(',');
-        var subject = options.subject || _subjectFromFilename(file.path);
-        var html = options.html || file.contents.toString();
-        var text = options.text || null;
-
-        return transporter.sendMail({
-            from: options.from,
-            to: to,
-            subject: subject,
-            generateTextFromHTML: true, // added
-            html: html,
-            text: text
-        }, function (error, info) {
-
-            if (error) {
-                log.error(error);
-                transporter.close();
-                return next();
-            }
-            
-            log.info('Sent email', colors.cyan(subject), 'to', colors.red(to));
-            transporter.close();
-            next();
-
-        });
-    });
-};
-
-_subjectFromFilename = function (filename) {
-    var name = path.basename(filename).replace(path.extname(filename), '');
-    return util.format('[TEST] %s', name);
+        transporter.close();
+        callback();
+      }
+    );
+  });
 };
