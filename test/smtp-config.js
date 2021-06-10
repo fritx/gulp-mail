b6b57683b23bbeffe42643b526ac3a02f27bb220
Jaroslav Vrana <jaroslav.vrana@integromat.com>
2021-06-10 22:35:42 +0200

    refactor(test): smtp config in standalone file
    Picked from #158c48fc75d0f86e643095320ae7b899485b54001 by justin713

---
+++ test/smtp-config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Replace the credentials below with your own in order to test
+module.exports = {
+  to: 'to@example.com',
+  from: 'user@example.com',
+  smtp: {
+    host: 'smtp.example.com',
+    port: 465,
+    secureConnection: true,
+    auth: {
+      user: 'user@example.com',
+      pass: 'correcthorsebatterystaple',
+    },
+  },
+};
